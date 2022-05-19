19bd6a067e0de8ad8bb85d3e916c7ed3ece79361
Annika <annikyet@icloud.com>
2022-05-19 12:03:42 -0600

    damage logic works

--- app.js
+++ app.js
@@ -26,6 +26,21 @@ class Hero {
     this.health = health
     this.mana = mana
     this.attacks = attacks
+    this.isAlive = true
+  }
+
+  attack(attack) {
+    let damage = this.attacks[attack].damage
+    boss.takeDamage(damage)
+  }
+
+  takeDamage(damage) {
+    if (damage >= this.health) {
+      this.health = 0
+      draw.death(this.id)
+    } else {
+      this.health -= damage
+    }
   }
 }
 
@@ -35,20 +50,36 @@ class Enemy {
     this.name = name
     this.health = health
     this.attacks = attacks
+    this.isAlive = true
   }
   
   attack() {
+    let attack = Math.floor(Math.random() * this.attacks.length)
+    let damage = this.attacks[attack].damage
+    hero.takeDamage(damage)
+  }
+
+  takeDamage(damage) {
+    if (damage >= this.health) {
+      this.health = 0
+      draw.death(this.id)
+    } else {
+      this.health -= damage
+    }
   }
 }
+
+class Draw {
+  death(id) {
+    console.log(id + ' dead...')
+  }
+}
+
+let draw = new Draw
 
 let scratch = new Attack('scratch', 'Scratch', 10, 90, 10)
 let bite = new Attack('bite', 'Bite', 20, 80, 20)
 let bark = new Attack('bark', 'Bark', 8, 80, 8)
 
-let hero =  new Hero('meow', 'Meowmeow', 120, 40, 20, {
-    scratch: scratch,
-    bite: bite
-  })
-
-let boss = new Enemy(
-  'doge', 'Doge', 120, [bark, bite])
+let hero = new Hero('meow', 'Meowmeow', 120, 20, {scratch: scratch, bite: bite})
+let boss = new Enemy('doge', 'Doge', 120, [bark, bite])
