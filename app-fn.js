255b8b00422b21a1ba04e66bd5beb6d3a708538e
Annika <annikyet@icloud.com>
2022-05-19 16:44:11 -0600

    de-factored WITHOUT classes

---
+++ app-fn.js
@@ -0,0 +1,140 @@
+let enemy = 'coke'
+let hero = 'monster'
+
+let attacks = {
+  explode: {
+    name: 'Explode',
+    baseDamage: 100,
+    hitRate: 10,
+    critRate: 10
+  },
+  puncture: {
+    name: 'Puncture',
+    baseDamage: 20,
+    hitRate: 80,
+    critRate: 20
+  },
+  fizzle: {
+    name: 'Fizzle',
+    baseDamage: 8,
+    hitRatee: 80,
+    critRate: 8
+  }
+}
+
+let combatants = {
+  coke: {
+    name: 'Coke',
+    party: 'enemy',
+    image: '',
+    baseHealth: 120,
+    maxHealth: 120,
+    health: 120,
+    level: 1,
+    isAlive: true,
+    attacks: [
+      'fizzle'
+    ]
+  },
+  monster: {
+    name: 'Monster',
+    party: 'hero',
+    image: 'https://images.heb.com/is/image/HEBGrocery/001621707-1?id=e7HRf1&amp;fmt=jpg&amp;dpr=off&amp;fit=constrain,1&amp;wid=196&amp;hei=196',
+    baseHealth: 120,
+    maxHealth: 120,
+    health: 120,
+    level: 1,
+    isAlive: true,
+    attacks: [
+      'explode',
+      'puncture'
+    ]
+  }
+}
+
+function calcDamage(attack, attacker) {
+  let damage = 0
+  if (Math.random() * 100 <= attacks[attack].hitRate) {
+    damage = (1 + (Math.random() - 0.5) * 0.4) * attacks[attack].baseDamage
+    if (Math.random() * 100 <= attacks[attack].critRate) {
+      damage += 2
+    }
+  }
+  return Math.floor(damage * combatants[attacker].level)
+}
+
+function getAttackHtml(attack) {
+  return `
+  <div class="col-6">
+  <button class="btn btn-primary w-100" onclick="attack('${attack}', 'monster', 'coke')">${attacks[attack].name}!</button>
+  </div>
+  `
+}
+
+function enemyDeath(combatant) {
+  const levelFactor = 1.2
+  combatants[combatant].level *= levelFactor
+  combatants[combatant].maxHealth = Math.floor(combatants[combatant].maxHealth * levelFactor)
+  combatants[combatant].health = combatants[combatant].maxHealth
+}
+
+function heroDeath(combatant) {
+  console.log('you ded')
+}
+
+function attack(attack, attacker, defender) {
+  if(combatants[attacker].isAlive) {
+    let damage = calcDamage(attack, attacker)
+    if (combatants[defender].health <= damage) {
+      if (combatants[defender].party === 'enemy') {
+        enemyDeath(defender)
+      } else if (combatants[defender].party === 'hero') {
+        heroDeath(defender)
+      }
+    } else {
+      combatants[defender].health -= damage
+    }
+    draw()
+  }
+}
+
+// document.getElementById('hero-card').innerHTML = getAttackHtml('explode')
+
+function getHeroCardHtml(combatant) {
+  let html = `
+      <div class="card">
+      <img src="https://thiscatdoesnotexist.com" alt="" class="card-img">
+      <div class="card-body">
+        <h2 class="card-title">${combatants[combatant].name}</h2>
+        <div class="progress">
+          <div class="progress-bar bg-danger" role="progressbar" style="width: ${100 * combatants[combatant].health / combatants[combatant].maxHealth}%;" 
+          ria-valuenow="${combatants[combatant].health}" aria-valuemin="0" aria-valuemax="${combatants[combatant].maxHealth}" id="boss-hp">${combatants[combatant].health}HP</div>
+        </div>
+        <div class="row mt-3">`
+
+  for (let a = 0; a < combatants[combatant].attacks.length; a++) {
+    html += getAttackHtml(combatants[combatant].attacks[a])
+  }
+  html += `</div></div></div>`
+
+  return html
+}
+
+function getEnemyCardHtml(combatant) {
+  return `
+      <div class="card">
+      <img src="https://thiscatdoesnotexist.com" alt="" class="card-img">
+      <div class="card-body">
+        <h2 class="card-title">${combatants[combatant].name}</h2>
+        <div class="progress">
+          <div class="progress-bar bg-danger" role="progressbar" style="width: ${100 * combatants[combatant].health / combatants[combatant].maxHealth}%;" 
+          ria-valuenow="${combatants[combatant].health}" aria-valuemin="0" aria-valuemax="${combatants[combatant].maxHealth}" id="boss-hp">${combatants[combatant].health}HP</div>
+        </div></div></div>`
+}
+
+function draw() {
+  document.getElementById('boss-card').innerHTML = getEnemyCardHtml(enemy)
+  document.getElementById('hero-card').innerHTML = getHeroCardHtml(hero)
+}
+
+draw()
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 class Attack {
   constructor(id, name, baseDamage, hitRate, critRate) {
-    this.id
+    this.id = id
     this.name = name
     this.baseDamage = baseDamage
     this.hitRate = hitRate
@@ -17,55 +17,114 @@ class Attack {
     }
     return Math.floor(damage)
   }
+  
+  get html() {
+    return `
+    <div class="col-6">
+    <button class="btn btn-primary w-100" onclick="control.heroAttack('${this.id}')">${this.name}!</button>
+    </div>
+    `
+  }
 }
 
 class Hero {
-  constructor(id, name, health, mana, attacks) {
+  constructor(id, name, image, health, attacks) {
     this.id = id
     this.name = name
-    this.health = health
-    this.mana = mana
-    this.attacks = attacks
-    this.isAlive = true
-  }
-
-  attack(attack) {
-    let damage = this.attacks[attack].damage
-    boss.takeDamage(damage)
-  }
-
-  takeDamage(damage) {
-    if (damage >= this.health) {
-      this.health = 0
-      draw.death(this.id)
-    } else {
-      this.health -= damage
-    }
-  }
-}
-
-class Enemy {
-  constructor(id, name, health, attacks) {
-    this.id = id
-    this.name = name
+    this.image = image
+    this.maxHealth = health
     this.health = health
     this.attacks = attacks
     this.isAlive = true
   }
   
+  attack(attack) {
+    if (this.isAlive) {
+      let damage = this.attacks[attack].damage
+      boss.takeDamage(damage)
+    }
+  }
+  
+  takeDamage(damage) {
+    if (damage >= this.health) {
+      this.health = 0
+      this.isAlive = false
+      draw.death(this.id)
+    } else {
+      this.health -= damage
+    }
+    draw.hero()
+  }
+  
+  get html() {
+    let attackHtml = ``
+    for (const a in this.attacks) {
+      attackHtml += this.attacks[a].html
+    }
+
+    return `
+    <div class="card">
+      <img src="https://thiscatdoesnotexist.com" alt="" class="card-img">
+      <div class="card-body">
+        <h2 class="card-title">${this.name}</h2>
+        <div class="progress">
+          <div class="progress-bar bg-danger" role="progressbar" style="width: ${100 * this.health / this.maxHealth}%;" 
+          ria-valuenow="${this.health}" aria-valuemin="0" aria-valuemax="${this.maxHealth}" id="boss-hp">${this.health}HP</div>
+        </div>
+        <div class="row mt-3">
+          ${attackHtml}
+        </div>
+      </div>
+    </div>
+    `
+  }
+}
+
+class Enemy {
+  constructor(id, name, image, health, attacks) {
+    this.id = id
+    this.name = name
+    this.image = image
+    this.maxHealth = health
+    this.level = 1
+    this.health = health
+    this.attacks = attacks
+  }
+  
   attack() {
+    console.log(this.attacks)
+    console.log(this.attacks.length)
+    debugger
     let attack = Math.floor(Math.random() * this.attacks.length)
-    let damage = this.attacks[attack].damage
+    console.log(attack)
+    console.log(this.attacks[attack])
+    let damage = Math.floor(this.attacks[attack].damage * this.level)
     hero.takeDamage(damage)
   }
 
   takeDamage(damage) {
     if (damage >= this.health) {
-      this.health = 0
-      draw.death(this.id)
+      this.level *= 1.2
+      this.health = Math.floor(this.maxHealth * this.level)
     } else {
       this.health -= damage
     }
+    draw.boss()
+  }
+
+  get html() {
+    return `
+      <div class="card">
+        <img src="${this.image}" alt="" class="card-img">
+        <div class="card-body">
+          <h2 class="card-title">Monster</h2>
+          <div class="progress">
+            <div class="progress-bar bg-danger" role="progressbar" style="width: ${100 * this.health / (this.maxHealth * this.level)}%;"
+            aria-valuenow="${this.health}" aria-valuemin="0" aria-valuemax="${Math.floor(this.maxHealth * this.level)}" id="boss-hp">${this.health}HP</div>
+          </div>
+        </div>
+      </div>
+    `
   }
 }
 
@@ -73,11 +132,21 @@ class Draw {
   death(id) {
     console.log(id + ' dead...')
   }
+
+  hero() {
+    document.getElementById('hero-card').innerHTML = hero.html
+  }
+
+  boss() {
+    document.getElementById('boss-card').innerHTML = boss.html
+  }
 }
 
 class Control {
   heroAttack(id) {
     hero.attack(id)
+    console.log('hero hp: ' + hero.health)
+    console.log('boss hp: ' + boss.health)
   }
 }
 
@@ -88,5 +157,16 @@ let scratch = new Attack('scratch', 'Scratch', 10, 90, 10)
 let bite = new Attack('bite', 'Bite', 20, 80, 20)
 let bark = new Attack('bark', 'Bark', 8, 80, 8)
 
-let hero = new Hero('meow', 'Meowmeow', 120, 20, {scratch: scratch, bite: bite})
-let boss = new Enemy('doge', 'Doge', 120, [bark, bite])
+let hero = new Hero('meow', 'Meowmeow', 'https://thiscatdoesnotexist.com', 120, {scratch: scratch, bite: bite})
+let boss = new Enemy('monster', 'Monster', 'https://images.heb.com/is/image/HEBGrocery/001621707-1?id=e7HRf1&amp;fmt=jpg&amp;dpr=off&amp;fit=constrain,1&amp;wid=196&amp;hei=196', 120, [bark, bite])
+
+draw.hero()
+draw.boss()
+
+function bossAttacks() {
+  boss.attack
+}
+
+// setInterval(boss.attack, 1000)
+
+// boss.attack()
